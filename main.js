97f178ab9d2754f1a1df3cb39bd418eeb156258a
Don Alcantara <don.alcantara@yahoo.com>
2016-09-01 16:22:58 -0700

    added notes

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-var myReactComponent = React.createClass({
+var MyReactComponent = React.createClass({
 	render: function(){
-		console.log(this.props)
-
-		var coloredHeadlines = this.props.colors.map(function(color,idx){
-			return React.createElement('h1', {style: {color: color}}, color + ' headline')
+		var coloredHeadlines = this.props.colors.map(function(color,idx){ //props shows what objects it has to work with, in this case it's colors.
+			return React.createElement('h1', {style: {color: color}, key: idx}, color + ' headline') //this creates an element with the attributes, colors, and children
 		})
-		return React.createElement('div', null, coloredHeadlines);
-	}
+		return React.createElement('div', null, coloredHeadlines);//this returns a div element with the coloredHeadlines variable as the child to show in the html div
+	} //end of function
 })
 
-var actualDOMNode = document.getElementById('app');
+var actualDOMNode = document.getElementById('app'); //this variable locates which id will show this code in the HTML page
 
-ReactDOM.render(React.createElement(myReactComponent, {colors: ['blue', 'gray', 'green']}), actualDOMNode);
+ReactDOM.render(React.createElement(MyReactComponent, {colors: ['Blue', 'Silver', 'Green']}), actualDOMNode); //render makes all the above code happen and where
